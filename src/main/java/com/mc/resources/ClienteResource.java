416330406d5d0cdcf45d0cc5e153ac2b82f506a3
Manoel C <manoelclemente9@outlook.com>
2020-03-07 21:17:49 -0300

    Endpoint /clientes/{id}

---
+++ src/main/java/com/mc/resources/ClienteResource.java
@@ -0,0 +1,40 @@
+package com.mc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mc.domain.Cliente;
+import com.mc.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+
+	@GetMapping
+	public List<Cliente> findAll(){
+		List<Cliente> obj = service.findAll();
+		return obj;
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		
+		Cliente obj = service.find(id);
+		
+		return ResponseEntity.ok().body(obj);	
+	
+	}	
+	
+
+}
---
+++ src/main/java/com/mc/services/ClienteService.java
@@ -0,0 +1,29 @@
+package com.mc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mc.domain.Cliente;
+import com.mc.repositories.ClienteRepository;
+import com.mc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+}
