4538acf97aeec3599a6fc979bd8027e744639ded
Manoel C <manoelclemente9@outlook.com>
2020-03-07 21:39:04 -0300

    Pedido, EstadoPagamento e Pagamento parte 1

---
+++ src/main/java/com/mc/domain/PagamentoComCartao.java
@@ -0,0 +1,30 @@
+package com.mc.domain;
+
+import java.io.Serializable;
+
+import com.mc.domain.enums.EstadoPagamento;
+
+public class PagamentoComCartao extends Pagamento implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer numeroDeParcelas;
+
+	public PagamentoComCartao() {
+
+	}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
+		super(id, estado, pedido);
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+
+}
