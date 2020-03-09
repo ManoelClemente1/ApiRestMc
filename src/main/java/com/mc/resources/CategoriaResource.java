d3401ff0181fa0c484a28634bc739b11a4cd4bbd
Manoel C <manoelclemente9@outlook.com>
2020-03-08 22:26:56 -0300

    Inserindo categoria com POST

--- src/main/java/com/mc/resources/CategoriaResource.java
+++ src/main/java/com/mc/resources/CategoriaResource.java
@@ -1,5 +1,6 @@
 package com.mc.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -8,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mc.domain.Categoria;
 import com.mc.services.CategoriaService;
@@ -35,6 +39,16 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);	
 	
 	}	
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+				
+		 
+	}
 	
 
 }
