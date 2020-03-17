c7d6a970d6d43b0050948bf94a76cb17a1299ed7
Manoel C <manoelclemente9@outlook.com>
2020-03-16 21:26:34 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/com/mc/resources/exceptions/ValidationError.java
@@ -0,0 +1,24 @@
+package com.mc.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StantardError {
+	private static final long serialVersionUID = 1L;
+
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public ValidationError(Integer status, String msg, Long timStamp) {
+		super(status, msg, timStamp);
+
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+
+}
