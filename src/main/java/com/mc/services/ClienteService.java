9aedfa3321c2d9544b3b966a45247cab14a410a4
Manoel C <manoelclemente9@outlook.com>
2020-03-18 22:38:07 -0300

    Ajustando delecao de cliente com cascadeType

--- src/main/java/com/mc/services/ClienteService.java
+++ src/main/java/com/mc/services/ClienteService.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mc.domain.Cidade;
 import com.mc.domain.Cliente;
@@ -41,6 +42,7 @@ public class ClienteService {
 
 	}
 	
+	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
 		obj= repo.save(obj);
